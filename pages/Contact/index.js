fe371e6a315aa63ab1125d8335156bbcbc0892f4
PaulRusz <paulruszkay@gmail.com>
2024-06-24 19:00:29 -0400

    added contact functionality where the contact form emails me personally

--- pages/Contact/index.js
+++ pages/Contact/index.js
@@ -2,7 +2,8 @@ import styles from './Contact.module.scss';
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import { motion } from 'framer-motion';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
 
@@ -28,85 +29,94 @@ export default function Contact() {
                 // Handle form validation errors
                 return;
             }
-
-            // try {
-            //     const response = await axios.post('http://_____________________my backend api url/contact' formData);
-            //     console.log(response.data) // Assuming the backend responds with a success message
-            //     setIsFormSubmitted(true)
     
             // Form data is valid, proceed with submission
             localStorage.setItem('formData', JSON.stringify(formData))
             setIsFormSubmitted(true)
-    
-            // Reset form fields after submission
-            setFormData({
-                name: '',
-                email: '',
-                message: ''
-            })
-    
-            // Clear "Message sent!" text after a delay
-            setTimeout(() => {
-                setIsFormSubmitted(false)
-            }, 3000);
         
     
             // Render the last submission to the page
             console.log(formData)
         }
-    
-        return (
-    
-            // Code for 'fade in' transition
-            <motion.div
+
+    // EMAILJS - send email from contact form
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('service_w4b10i6', 'template_kg2bmq2', form.current, {
+                publicKey: '7fbEPFKwQg92_gfOL',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                    // Reset form fields after submission
+                    setFormData({
+                        name: '',
+                        email: '',
+                        message: ''
+                    })
+
+                    // Clear "Message sent!" text after a delay
+                    setTimeout(() => {
+                        setIsFormSubmitted(false)
+                    }, 3000);
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
+
+    return (
+        // Code for 'fade in' transition
+        <motion.div
             className="page1"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1, transition: { duration: 1 } }}
             exit={{ opacity: 0 }}
-          >
+        >
+            <Navbar />
 
-        <Navbar />
-    
             <div className={styles.contactContainer}>
                 <h1>Contact Me</h1>
-    
+
                 <div className='divider'></div>
-    
-                <p> Have a question or want to get in touch? </p>  
-                
+
+                <p> Have a question or want to get in touch? </p>
+
                 <p> Feel free to reach out! </p>
                 <p>
                     Fill out the form below, email me, or checkout one of the links below.
                 </p>
-    
-                <form className={styles.contactForm} onSubmit={handleSubmit}> 
-                {isFormSubmitted ? (
-                        <motion.p 
-                            className="messageSent"
+
+                <form className={styles.contactForm} ref={form}>
+                    {isFormSubmitted && (
+                        <motion.p
+                            className={styles.model}
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             exit={{ opacity: 0 }}
                             transition={{ duration: 1 }}
                         >
-                            Message sent!
+                            <p> Message sent! </p>
                         </motion.p>
-                    ) : null}
+                    )}
                     <p> Send me a message </p>
                     <input className={styles.name} type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Your Name" ></input>
                     <input className={styles.email} type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Your Email" ></input>
                     <textarea className={styles.message} name="message" value={formData.message} onChange={handleChange} placeholder="Message..." ></textarea>
-                    <button className={styles.button} type="submit"> Send </button>
-    
+                    <button className={styles.button} type="submit" onClick={sendEmail}> Send </button>
                 </form>
-    
+
                 <div className="contactInfo">
-                <ul><a href="mailto:PaulRuszkay@gmail.com"> 📧 Email:  PaulRuszkay@gmail.com </a></ul>
-                <ul><a href="https://github.com/PaulRusz"> 📁 GitHub: github.com/PaulRusz </a></ul>
-                <ul><img src={linkedInImage} className={styles.linkedIn} alt="LinkedIn"></img><a href="https://www.linkedin.com/in/paul-ruszkay-a2ba075b/"> LinkedIn - Paul Ruszkay </a></ul>
+                    <ul><a href="mailto:PaulRuszkay@gmail.com"> 📧 Email:  PaulRuszkay@gmail.com </a></ul>
+                    <ul><a href="https://github.com/PaulRusz"> 📁 GitHub: github.com/PaulRusz </a></ul>
+                    <ul><img src={linkedInImage} className={styles.linkedIn} alt="LinkedIn"></img><a href="https://www.linkedin.com/in/paul-ruszkay-a2ba075b/"> LinkedIn - Paul Ruszkay </a></ul>
                 </div>
-    
             </div>
-    
-            </motion.div>
-        )
-    }
+        </motion.div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,26 @@ import React from 'react'
 import About from './About/index.js'
 import Navbar from '../components/Navbar/Navbar.js';
 import Link from 'next/link'
+import emailjs from '@emailjs/browser';
 
 export default function Home () {
   return (
+    
     <div>
+        <div> 
+        <script type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
+        </script>
+        <script type="text/javascript">
+        (function(){
+      emailjs.init({
+        publicKey: "7fbEPFKwQg92_gfOL",
+         })
+        })();
+        </script>
+        </div>
          <Navbar />
     <div></div>
-
     </div>
   )
 }
