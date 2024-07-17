a8dd6e7349eb0110f5b8c9694d3ff08fcacd12b9
PaulRusz <paulruszkay@gmail.com>
2024-07-16 21:26:54 -0400

    added workout2.0 to portfolio area

--- pages/Portfolio/index.js
+++ pages/Portfolio/index.js
@@ -23,29 +23,39 @@ return (
         <h1>Portfolio</h1>
         <div className={styles.divider}></div>
         <h2 className={styles.header2}>Here are some of my projects:</h2>
+
         <div className={styles.projectDivider}>
         <ul>
                 <a className={styles.link} href="https://friendsblog.onrender.com/" target="_blank" rel=""> FriendsBlog <br></br>
                 <Image src="/FriendsBlog.png" alt="Friends Blog" width={600} height={300} />
                 </a>
-                
         </ul>
         <p> A social media / blogging platform where a user creates posts, search for other users, and view other user profiles. </p>
         <p> FriendsBlog utilized React, Mongo DB Atlas, Apollo, graphSQL, Font-Awesome, NPM Packages, Vite Config.  </p>
         <p>Refer to the <a href="Github: https://github.com/PaulRusz/FriendsBlog">FriendsBlog</a> GitHub for further on what I personally accomplished for this project.</p>
         </div>
+
         <div className={styles.divider2}></div>
         <ul>
                 <a href="https://github.com/PaulRusz/Personal-Note-Taker" target="_blank" rel=""> Personal Note App </a>
         </ul>
+
         <div className={styles.divider2}></div>
+        <div className={styles.projectDivider}>
         <ul>
-                <a href="https://github.com/PaulRusz/Group-Project1" target="_blank" rel=""> Workout Tracker</a>
+                <a href="hhttps://paulrusz.github.io/Group-Project1/" target="_blank" rel=""> Workout Tracker 1.0 <br></br>
+                <Image src="/Workout1.0.png" alt="Workout App 1.0" width={600} height={300} /></a>
         </ul>
+        </div>
+
         <div className={styles.divider2}></div>
+        <div className={styles.projectDivider}>
         <ul>
-                <a href="https://workout-tracker-3b0i.onrender.com/" target="_blank" rel=""> Workout Tracker 2.0</a>
+                <a href="https://workout-tracker-3b0i.onrender.com/" target="_blank" rel=""> Workout Tracker 2.0 <br></br>
+                <Image src="/Workout2.png" alt="Workout App 2.0" width={600} height={300} /></a>
         </ul>
+        </div>
+        
         <div className={styles.divider2}></div>
         <ul>
                 <a href="https://github.com/PaulRusz/project2" target="_blank" rel=""> Project 4</a>
