b5fc74decebd4edb648463a6ed5a3db060aaf615
PaulRusz <paulruszkay@gmail.com>
2024-07-31 14:49:44 -0400

    added to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/global.scss'
 import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer.js';
 
 
 export default function App ({ Component, pageProps }) {
-    return <Component {...pageProps} />
-    
+    return (
+    <div>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+    </div>
+    )
 }
