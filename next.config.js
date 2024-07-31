080a6e72fd3d99a2bf9be01eb8d5a66cbdef5969
PaulRusz <paulruszkay@gmail.com>
2024-07-31 14:37:59 -0400

    changed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,34 @@
+// module.exports = {
+//     async rewrites() {
+//         return [
+//             {
+//                 source: '/',
+//                 destination: '/about',
+//             },
+//         ];
+//     },
+// };
+
+
 module.exports = {
     async rewrites() {
         return [
             {
-                source: '/',
-                destination: '/about',
+                source: '/about',
+                destination: '/pages/about/index',
             },
-        ];
-    },
+            {
+                source: '/contact',
+                destination: '/pages/contact/index',
+            },
+            {
+                source: '/portfolio',
+                destination: '/pages/portfolio/index',
+            },
+            {
+                source: '/resume',
+                destination: '/pages/resume/index',
+            }
+        ]
+    }
 };
